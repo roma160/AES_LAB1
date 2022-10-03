021fd81937028c44e0936b91d8abb9c7baa0ac69
super mario <mario160@mail.ru>
2022-10-04 00:52:40 +0300

    Backup todo

--- LAB1.cpp
+++ LAB1.cpp
@@ -30,7 +30,7 @@ __forceinline void exec_operation(const T a, const T b, volatile T& res, operati
 { res = a / b; }
 template<typename T>
 __forceinline void exec_operation(const T a, const T b, volatile T& res, operation_t<no_operation>)
-{ res = a; }
+{ res = a; res = b; }
 
 template<typename T, const unsigned int end, operation O, const unsigned int start = 0>
 __forceinline void exec_operation(volatile T a, volatile T b, volatile T& res, const operation_t<O>& op)
@@ -54,21 +54,35 @@ double exec(const _loop_t count = 100)
 	}
 
 	const auto end = chrono::high_resolution_clock::now();
-	return (double)chrono::duration_cast<chrono::milliseconds>(end - begin).count();
+	return (double)chrono::duration_cast<chrono::nanoseconds>(end - begin).count();
 }
 
 
+// TODO: fix check function
 template <operation O, typename T, const unsigned int repeat = 20>
 double check(const _loop_t count = 100)
 {
-	double micros_op = exec<O, T, repeat>(count);
-	double micros_no = exec<no_operation, T, repeat>(count);
-	return count * 1e6 * repeat / (micros_op);
+	double t_op1 = 1, t_op2 = 0, t_no2 = 0, t_no1 = 0, delta;
+	while((delta = t_op2 - t_op1 - t_no2) <= 0)
+	{
+		t_op1 = exec<O, T, repeat>(count);
+		t_op2 = exec<O, T, 2*repeat>(count);
+		t_no1 = exec<no_operation, T, 1>(count);
+		t_no2 = exec<no_operation, T, repeat>(count);
+	}
+	return count * 1e9 * repeat / delta;
 }
+
+
 
 int main()
 {
 	cout.precision(4);
-	cout << check<addition, int>(1e8);
+	double r = 1e11;
+	while(r > 1e10)
+	{
+		r = check<addition, int, 5>(1e3);
+		cout << r << "\n";
+	}
 	return 0;
 }
