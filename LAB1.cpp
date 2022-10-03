a0fdc5a67bfe1630498d101f283aa547f9a7195e
super mario <mario160@mail.ru>
2022-10-03 22:01:05 +0300

    Measure PoC

--- LAB1.cpp
+++ LAB1.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <chrono>
 
 using namespace std;
 
@@ -7,7 +8,8 @@ enum operation
 	addition,
 	subtraction,
 	multiplication,
-	division
+	division,
+	no_operation
 };
 
 // https://stackoverflow.com/questions/11430015/how-to-guarantee-that-a-function-is-inlined-in-c
@@ -26,18 +28,32 @@ __forceinline T exec_operation(T a, T b, operation_t<multiplication>)
 template<typename T>
 __forceinline T exec_operation(T a, T b, operation_t<division>)
 { return a / b; }
+template<typename T>
+__forceinline T exec_operation(T a, T b, operation_t<no_operation>)
+{ return a; }
 
-// https://learn.microsoft.com/en-us/cpp/preprocessor/optimize?view=msvc-170
-#pragma optimize("", off)
-int exec()
+
+template <operation O, typename T>
+double exec(const unsigned int count = 100)
 {
-	// https://stackoverflow.com/questions/37980791/conditional-function-invocation-using-template
-	return exec_operation(1, 2, operation_t<multiplication>());
+	const auto begin = chrono::high_resolution_clock::now();
+
+	volatile T res = 0, b = 1;
+	for (unsigned int i = 0; i < count; i++) {
+		// https://stackoverflow.com/questions/37980791/conditional-function-invocation-using-template
+		res = exec_operation<T>(res, b, operation_t<O>());
+	}
+
+	const auto end = chrono::high_resolution_clock::now();
+	return (double)chrono::duration_cast<chrono::milliseconds>(end - begin).count();
 }
-#pragma optimize("", on)
 
 int main()
-{ 
-	cout << exec();
+{
+	cout.precision(4);
+	double operations = 1e7;
+	double micros_op = exec<addition, int>(operations);
+	double micros_no = exec<no_operation, int>(operations);
+	cout << operations * 1e6 / (micros_op - micros_no);
     return 0;
 }
