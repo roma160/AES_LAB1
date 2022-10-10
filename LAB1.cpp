75b9eca6c5d7ba60214ba561c0d5e66c40f884f9
super mario <mario160@mail.ru>
2022-10-10 23:09:02 +0300

    Backup refactor

--- LAB1.cpp
+++ LAB1.cpp
@@ -17,57 +17,70 @@ enum operation
 template <operation t>
 using operation_t = integral_constant<operation, t>;
 
-template<typename T>
-__forceinline void exec_operation(const T a, const T b, volatile T& res, operation_t<addition>)
-{
-	res = a + b;
-}
-template<typename T>
-__forceinline void exec_operation(const T a, const T b, volatile T& res, operation_t<subtraction>)
-{
-	res = a - b;
-}
-template<typename T>
-__forceinline void exec_operation(const T a, const T b, volatile T& res, operation_t<multiplication>)
-{
-	res = a * b;
-}
-template<typename T>
-__forceinline void exec_operation(const T a, const T b, volatile T& res, operation_t<division>)
-{
-	res = a / b;
-}
-template<typename T>
-__forceinline void exec_operation(const T a, const T b, volatile T& res, operation_t<no_operation>)
-{
-	res = a; res = b;
-}
+template <typename T>
+using op_arg_t = T;
 
-template<typename T, const unsigned int end, operation O, const unsigned int start = 0>
+template<typename T>
+__forceinline void exec_operation(op_arg_t<T> a, op_arg_t<T> b, volatile T& res, operation_t<addition>)
+{ res = a + b; }
+template<typename T>
+__forceinline void exec_operation(op_arg_t<T> a, op_arg_t<T> b, volatile T& res, operation_t<subtraction>)
+{ res = a - b; }
+template<typename T>
+__forceinline void exec_operation(op_arg_t<T> a, op_arg_t<T> b, volatile T& res, operation_t<multiplication>)
+{ res = a * b; }
+template<typename T>
+__forceinline void exec_operation(op_arg_t<T> a, op_arg_t<T> b, volatile T& res, operation_t<division>)
+{ res = a / b; }
+template<typename T>
+__forceinline void exec_operation(op_arg_t<T> a, op_arg_t<T> b, volatile T& res, operation_t<no_operation>)
+{ res = a; }
+
+
+template<typename T, operation O, const size_t R, enable_if_t<0 == R, bool> = 0>
+__forceinline void exec_operation(volatile T a, volatile T b, volatile T& res, const operation_t<O>& op)
+{ exec_operation(a, b, res, op); }
+
+template<typename T, operation O, const size_t R, enable_if_t<0 < R, bool> = 0>
 __forceinline void exec_operation(volatile T a, volatile T b, volatile T& res, const operation_t<O>& op)
 {
 	exec_operation(a, b, res, op);
-	if constexpr (start + 1 < end)
-		exec_operation<T, end, O, start + 1>(a, b, res, op);
+	exec_operation<T, O, R - 1>(a, b, res, op);
+}
+
+enum use_grounding
+{ yes, no };
+template <use_grounding t>
+using ground_t = integral_constant<use_grounding, t>;
+
+template<typename T, operation O, const size_t R>
+__forceinline void exec_operation(volatile T a, volatile T b, volatile T& res, const operation_t<O>& op, const ground_t<yes>&)
+{
+	exec_operation<T, O, R>(a, b, res, op);
+	
+}
+template<typename T, operation O, const size_t R>
+__forceinline void exec_operation(volatile T a, volatile T b, volatile T& res, const operation_t<O>& op, const ground_t<yes>&)
+{
+	exec_operation(a, b, res, op);
+	exec_operation<T, O, R - 1>(a, b, res, op);
 }
 
 typedef unsigned long long _loop_t;
-template <operation O, typename T, const unsigned int repeat>
+template <operation O, typename T, const unsigned int R>
 double exec(const _loop_t count = 100)
 {
 	const operation_t<O> op_t{};
-	LARGE_INTEGER freq, begg, end;
-	QueryPerformanceFrequency(&freq);
-	QueryPerformanceCounter(&begg);
+	const auto begin = chrono::high_resolution_clock::now();
 
 	T r = 0, a = 1, b = 1;
 	for (_loop_t i = 0; i < count; i++) {
 		// https://stackoverflow.com/questions/37980791/conditional-function-invocation-using-template
-		exec_operation<T, repeat>(a, b, r, op_t);
+		exec_operation<T, O, R>(a, b, r, op_t);
 	}
 
-	QueryPerformanceCounter(&end);
-	return (double)(end.QuadPart - begg.QuadPart) / freq.QuadPart;
+	const auto end = chrono::high_resolution_clock::now();
+	return (double) chrono::duration_cast<chrono::nanoseconds>(end - begin).count();
 }
 
 
@@ -87,30 +100,28 @@ double check(const _loop_t count = 100)
 }
 
 
-double e(const _loop_t count = 100)
-{
-	bool res = true;
-	LARGE_INTEGER freq, begg, end;
-	res = QueryPerformanceFrequency(&freq);
-	res = QueryPerformanceCounter(&begg);
-
-	int a = 1, b = 1, c = 1, d = 1;
-	for (_loop_t i = 0; i < count; i++) {
-		a = b + c;
-		c = a + d;
-		a += c;
-		b += d;
-	}
-
-	QueryPerformanceCounter(&end);
-	double s = (double)(end.QuadPart - begg.QuadPart) / freq.QuadPart;
-	return s;
-}
-
-
 int main()
 {
 	cout.precision(4);
-	cout << check<addition, int>(1e5);
+
+	for (int i = 0; i < 5; i++) {
+		double sum = 0;
+		const size_t l = 1e2;
+		for(int i = 0; i < l; i++)
+			sum += exec<addition, int, 100>(10);
+		sum /= l;
+		cout << sum;
+
+		cout << "\n";
+
+		double _sum = sum;
+
+		sum = 0;
+		for (int i = 0; i < l; i++)
+			sum += exec_g<addition, int, 100>(10);
+		sum /= l;
+		cout << sum << "\n";
+		cout << sum - _sum <<"\n||||||||||||||||||\n";
+	}
 	return 0;
 }
